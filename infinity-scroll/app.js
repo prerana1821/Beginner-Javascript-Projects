60a7fedc209a06815d66c7687daff82f13aa95e3
prerana1821 <prerananawar1@gmail.com>
2021-01-02 19:44:49 +0530

    update: added responsiveness & infinity scroll functionality

--- infinity-scroll/app.js
+++ infinity-scroll/app.js
@@ -3,7 +3,9 @@ let searchBtn = document.querySelector('#search-btn');
 let imageContainer = document.querySelector('.image-container');
 let selectList = document.getElementById("select-list");
 
-let apiKey = '563492ad6f917000010000013399437d33f545b6ad303f4f00c8745c';
+// let apiKey = '563492ad6f917000010000013399437d33f545b6ad303f4f00c8745c';
+// let apiKey = '563492ad6f91700001000001eea6e7a56c3a4c8eb856a6ad623afa26';
+let apiKey = '563492ad6f917000010000017d374ccff7504daf914afcf880774e85';
 
 // "https://api.pexels.com/v1/search?query=nature&per_page=1"
 function contructURL(imgQuery) {
@@ -45,6 +47,7 @@ function getImages() {
                         let div = document.createElement('div');
                         // img.appendChild(link);
                         // imageContainer.appendChild(img);
+
                         // setTimeout(function() {
                         //     loader.className = loader.className.replace("show", "");
                         //     imageContainer.className = output.className.replace("hide", "");
@@ -64,11 +67,13 @@ function getImages() {
     };
 }
 
+let selectedValue = '';
 selectList.addEventListener("click", function(e) {
     if (e.target && e.target.nodeName == "LI") {
         // console.log(e.target.id);
+        selectedValue = e.target.id;
         imageContainer.innerHTML = '';
-        fetch(contructURL(e.target.id), {
+        fetch(contructURL(selectedValue), {
                 method: 'GET',
                 headers: {
                     'Authorization': apiKey,
@@ -103,76 +108,79 @@ selectList.addEventListener("click", function(e) {
                 console.log(error);
                 alert('To Many Request, Please try again later');
             });
-
-
-        window.addEventListener('scroll', () => {
-            if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 10000) {
-                // fetch(contructURL(inputImg.value === '' ? inputImg.value : e.target.id), {
-                fetch(contructURL(inputImg.value), {
-                        method: 'GET',
-                        headers: {
-                            'Authorization': apiKey,
-                        },
-                        body: JSON.stringify(),
-                    })
-                    .then(response => response.json())
-                    .then(function(data) {
-                        let photos = data.photos;
-                        // console.log(photos);
-                        photos.forEach(photo => {
-                            let link = document.createElement('a');
-                            link.setAttribute('href', photo.url);
-                            link.setAttribute('target', '_blank');
-                            link.setAttribute('title', photo.photographer)
-                            let img = document.createElement('img');
-                            img.setAttribute('src', photo.src.original);
-                            img.setAttribute('alt', photo.photographer);
-                            img.setAttribute('title', photo.photographer);
-                            img.setAttribute('class', 'photos');
-                            let div = document.createElement('div');
-                            link.appendChild(img);
-                            div.appendChild(link);
-                            imageContainer.appendChild(div);
-                            // console.log(img);
-                            // console.log(link);
-                        });
-                    });
-            }
-        });
     }
 });
 
-window.addEventListener('scroll', () => {
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 10000) {
-        // fetch(contructURL(inputImg.value === '' ? inputImg.value : e.target.id), {
-        fetch(contructURL(inputImg.value), {
-                method: 'GET',
-                headers: {
-                    'Authorization': apiKey,
-                },
-                body: JSON.stringify(),
-            })
-            .then(response => response.json())
-            .then(function(data) {
-                let photos = data.photos;
-                // console.log(photos);
-                photos.forEach(photo => {
-                    let link = document.createElement('a');
-                    link.setAttribute('href', photo.url);
-                    link.setAttribute('target', '_blank');
-                    link.setAttribute('title', photo.photographer)
-                    let img = document.createElement('img');
-                    img.setAttribute('src', photo.src.original);
-                    img.setAttribute('alt', photo.photographer);
-                    img.setAttribute('title', photo.photographer);
-                    img.setAttribute('class', 'photos');
-                    let div = document.createElement('div');
-                    link.appendChild(img);
-                    div.appendChild(link);
-                    imageContainer.appendChild(div);
-                    // console.log(img);
-                    // console.log(link);
-                });
-            });
-    }
-});
+// if (selectedValue.length >= 0) {
+//     window.addEventListener('scroll', () => {
+//         if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 10) {
+//             // fetch(contructURL(inputImg.value === '' ? inputImg.value : e.target.id), {
+//             fetch(contructURL(selectedValue), {
+//                     method: 'GET',
+//                     headers: {
+//                         'Authorization': apiKey,
+//                     },
+//                     body: JSON.stringify(),
+//                 })
+//                 .then(response => response.json())
+//                 .then(function(data) {
+//                     let photos = data.photos;
+//                     // console.log(photos);
+//                     photos.forEach(photo => {
+//                         let link = document.createElement('a');
+//                         link.setAttribute('href', photo.url);
+//                         link.setAttribute('target', '_blank');
+//                         link.setAttribute('title', photo.photographer)
+//                         let img = document.createElement('img');
+//                         img.setAttribute('src', photo.src.original);
+//                         img.setAttribute('alt', photo.photographer);
+//                         img.setAttribute('title', photo.photographer);
+//                         img.setAttribute('class', 'photos');
+//                         let div = document.createElement('div');
+//                         link.appendChild(img);
+//                         div.appendChild(link);
+//                         imageContainer.appendChild(div);
+//                         // console.log(img);
+//                         // console.log(link);
+//                     });
+//                 });
+//         }
+//     });
+
+// }
+
+
+// window.addEventListener('scroll', () => {
+//     if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 10) {
+//         // fetch(contructURL(inputImg.value === '' ? inputImg.value : e.target.id), {
+//         fetch(contructURL(inputImg.value), {
+//                 method: 'GET',
+//                 headers: {
+//                     'Authorization': apiKey,
+//                 },
+//                 body: JSON.stringify(),
+//             })
+//             .then(response => response.json())
+//             .then(function(data) {
+//                 let photos = data.photos;
+//                 // console.log(photos);
+//                 photos.forEach(photo => {
+//                     let link = document.createElement('a');
+//                     link.setAttribute('href', photo.url);
+//                     link.setAttribute('target', '_blank');
+//                     link.setAttribute('title', photo.photographer)
+//                     let img = document.createElement('img');
+//                     img.setAttribute('src', photo.src.original);
+//                     img.setAttribute('alt', photo.photographer);
+//                     img.setAttribute('title', photo.photographer);
+//                     img.setAttribute('class', 'photos');
+//                     let div = document.createElement('div');
+//                     link.appendChild(img);
+//                     div.appendChild(link);
+//                     imageContainer.appendChild(div);
+//                     // console.log(img);
+//                     // console.log(link);
+//                 });
+//             });
+//     }
+// });

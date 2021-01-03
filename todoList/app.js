dfd8cc58262de128ede4ae770aecfe99811ed82f
prerana1821 <prerananawar1@gmail.com>
2021-01-04 02:00:24 +0530

    Completed

--- todoList/app.js
+++ todoList/app.js
@@ -2,6 +2,7 @@ let inputTask = document.querySelector('.input-txt');
 let addBtn = document.querySelector('.btn-submit');
 let taskList = document.querySelector('.todo-lists');
 let date = document.querySelector('.date');
+let filterList = document.querySelector('.filter-todo');
 
 
 let dateObject = new Date();
@@ -12,16 +13,15 @@ inputTask.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
         event.preventDefault();
         addTodo(event);
-        // console.log('Hello');
     }
 });
 
 addBtn.addEventListener('click', addTodo);
 taskList.addEventListener('click', checkDelete);
+filterList.addEventListener('click', filterTodo);
 
 function addTodo(event) {
     event.preventDefault();
-    // console.log('Prerana');
 
     if (inputTask.value.length <= 0) {
         alert('Please, Enter some text!')
@@ -29,11 +29,20 @@ function addTodo(event) {
         let todoItem = document.createElement('div');
         todoItem.classList.add('todo-item');
 
+        let leftDiv = document.createElement('div');
+        leftDiv.classList.add('left-div');
+
+        let timeP = document.createElement('p');
+        timeP.innerText = formatAMPM(new Date);
+        timeP.classList.add('time');
+
         let todoLi = document.createElement('li');
         todoLi.innerText = inputTask.value;
         todoLi.classList.add('todo-list');
 
-        todoItem.appendChild(todoLi);
+        leftDiv.appendChild(timeP);
+        leftDiv.appendChild(todoLi);
+        todoItem.appendChild(leftDiv);
 
         let checkBtn = document.createElement('div');
         checkBtn.innerHTML = '<img class="btn-check-img" src="images/checked.gif"/>';
@@ -46,18 +55,17 @@ function addTodo(event) {
         todoItem.appendChild(deleteBtn);
 
         taskList.appendChild(todoItem);
-
         inputTask.value = '';
     }
 }
 
 function checkDelete(event) {
     let item = event.target;
-    console.log(item);
     if (item.classList[0] === 'btn-delete') {
-        // console.log(event.target.parentElement);
-        // event.target.parentElement.remove();
-        item.parentElement.remove();
+        item.parentElement.classList.add('fall');
+        item.parentElement.addEventListener('transitionend', function() {
+            item.parentElement.remove();
+        });
     } else if (item.classList[0] === 'btn-check') {
         item.parentElement.classList.toggle('task-completed');
     }
@@ -80,3 +88,169 @@ anime.timeline({ loop: true })
         easing: "easeOutExpo",
         delay: 1000
     });
+
+
+function filterTodo(event) {
+    let todos = taskList.children;
+    // console.log(todos);
+    for (let i = 0; i < todos.length; i++) {
+        // console.log(todos[i]);
+        switch (event.target.value) {
+            case "all":
+                todos[i].style.display = "flex";
+                break;
+            case "completed":
+                if (todos[i].classList.contains("task-completed")) {
+                    todos[i].style.display = "flex";
+                } else {
+                    todos[i].style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todos[i].classList.contains("task-completed")) {
+                    todos[i].style.display = "flex";
+                } else {
+                    todos[i].style.display = "none";
+                }
+        }
+    }
+    // console.log(event.target.value);
+    // todos.forEach(function(todo) {
+    //     console.log(todo);
+    //     console.log('HEllo');
+    //     switch (event.target.value) {
+    //         case "all":
+    //             todo.style.display = "flex";
+    //             break;
+    //         case "completed":
+    //             if (todo.classList.contains("task-completed")) {
+    //                 todo.style.display = "flex";
+    //             } else {
+    //                 todo.style.display = "none";
+    //             }
+    //             break;
+    //         case "uncompleted":
+    //             if (!todo.classList.contains("task-completed")) {
+    //                 todo.style.display = "flex";
+    //             } else {
+    //                 todo.style.display = "none";
+    //             }
+    //     }
+    // });
+}
+
+let todoTime;
+
+function formatAMPM(date) {
+    // console.log(date);
+    todoTime = date;
+    // console.log(todoTime);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    let strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+// console.log(formatAMPM(new Date));
+
+// function timeDiffCalc(dateFuture, dateNow) {
+//     let diffInMilliSeconds = Math.abs(dateFuture - dateNow) / 1000;
+
+//     // calculate days
+//     // const days = Math.floor(diffInMilliSeconds / 86400);
+//     // diffInMilliSeconds -= days * 86400;
+//     // console.log('calculated days', days);
+
+//     // calculate hours
+//     const hours = Math.floor(diffInMilliSeconds / 3600) % 24;
+//     diffInMilliSeconds -= hours * 3600;
+//     // console.log('calculated hours', hours);
+
+//     // calculate minutes
+//     const minutes = Math.floor(diffInMilliSeconds / 60) % 60;
+//     diffInMilliSeconds -= minutes * 60;
+//     // console.log('minutes', minutes);
+
+//     let difference = '';
+//     // if (days > 0) {
+//     //     difference += (days === 1) ? `${days} day, ` : `${days} days, `;
+//     // }
+
+//     difference += (hours === 0 || hours === 1) ? `${hours} hr, ` : `${hours} hr, `;
+
+//     difference += (minutes === 0 || hours === 1) ? `${minutes} min` : `${minutes} min`;
+
+//     return difference;
+// }
+
+// console.log(timeDiffCalc(new Date(), todoTime));
+
+// the time difference is:
+// 1 day, 14 hours, 10 minutes
+
+
+// function addTodo(event) {
+//     event.preventDefault();
+
+//     if (inputTask.value.length <= 0) {
+//         alert('Please, Enter some text!')
+//     } else {
+//         let todoItem = document.createElement('div');
+//         todoItem.classList.add('todo-item');
+
+//         let leftDiv = document.createElement('div');
+//         leftDiv.classList.add('left-div');
+
+//         let timeP = document.createElement('p');
+//         timeP.innerText = formatAMPM(new Date);
+//         timeP.classList.add('time');
+
+//         // let diffTime = document.createElement('span');
+//         // diffTime.innerText = timeDiffCalc(new Date(), todoTime);
+//         // diffTime.classList.add('diff-time');
+
+//         // timeP.appendChild(diffTime);
+
+//         let todoLi = document.createElement('li');
+//         todoLi.innerText = inputTask.value;
+//         todoLi.classList.add('todo-list');
+
+//         leftDiv.appendChild(timeP);
+//         leftDiv.appendChild(todoLi);
+//         todoItem.appendChild(leftDiv);
+//         // todoItem.appendChild(todoLi);
+
+//         let checkBtn = document.createElement('div');
+//         checkBtn.innerHTML = '<img class="btn-check-img" src="images/checked.gif"/>';
+//         checkBtn.classList.add('btn-check');
+//         todoItem.appendChild(checkBtn);
+
+//         let deleteBtn = document.createElement('div');
+//         deleteBtn.innerHTML = '<img class="btn-delete-img" src="images/deleted.gif"/>';
+//         deleteBtn.classList.add('btn-delete');
+//         todoItem.appendChild(deleteBtn);
+
+//         taskList.appendChild(todoItem);
+
+//         inputTask.value = '';
+//     }
+// }
+
+// function checkDelete(event) {
+//     let item = event.target;
+//     // console.log(item);
+//     if (item.classList[0] === 'btn-delete') {
+//         // console.log(event.target.parentElement);
+//         // event.target.parentElement.remove();
+//         item.parentElement.classList.add('fall');
+//         item.parentElement.addEventListener('transitionend', function () {
+//             item.parentElement.remove();
+//         });
+//     } else if (item.classList[0] === 'btn-check') {
+//         item.parentElement.classList.toggle('task-completed');
+//     }
+// }

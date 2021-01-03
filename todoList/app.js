da718f6cd247018c9005b5b625d6161536819a8c
prerana1821 <prerananawar1@gmail.com>
2021-01-03 05:32:35 +0530

    update: Added add task functionality

--- todoList/app.js
+++ todoList/app.js
@@ -1,3 +1,55 @@
+let inputTask = document.querySelector('.input-txt');
+let addBtn = document.querySelector('.btn-submit');
+let taskList = document.querySelector('.todo-list');
+
+
+inputTask.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        addTodo(event);
+        console.log('Hello');
+    }
+});
+
+addBtn.addEventListener('click', addTodo);
+
+function addTodo(event) {
+    event.preventDefault();
+    console.log('Preraana');
+
+    if (inputTask.value.length <= 0) {
+        alert('Please, Enter some text!')
+    } else {
+        let todoItem = document.createElement('div');
+        todoItem.classList.add('todo-item');
+
+        let todoLi = document.createElement('li');
+        todoLi.innerText = inputTask.value;
+        todoLi.classList.add('todo-list');
+
+        todoItem.appendChild(todoLi);
+
+        let checkBtn = document.createElement('div');
+        checkBtn.innerHTML = '<img src="images/checked.gif"/>';
+        checkBtn.classList.add('btn-check');
+        todoItem.appendChild(checkBtn);
+
+        let deleteBtn = document.createElement('div');
+        deleteBtn.innerHTML = '<img src="images/deleted.gif"/>';
+        deleteBtn.classList.add('btn-delete');
+        todoItem.appendChild(deleteBtn);
+
+        taskList.appendChild(todoItem);
+
+        inputTask.value = '';
+    }
+
+}
+
+
+
+
+
 var textWrapper = document.querySelector('.ml9 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
